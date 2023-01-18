fb073c73cfabfc6da7b0f4d4155aefb066be2fe8
Linlin Deng <linlin.deng@icemedia.com.au>
2023-01-18 15:43:47 +1000

    update

--- Program.cs
+++ Program.cs
@@ -76,7 +76,10 @@ namespace CSharpPrac
             //Console.WriteLine(sumResult);
             Console.WriteLine(Add(Add(1, 2), Add(3, 4)));
             Console.WriteLine(Add(15, 31));
+            Console.WriteLine(Multiply(25, 25));
+            Console.WriteLine(Devide(25,13));
             Console.Read();
+
         }
 
         // acess modifier (static) return type method name (parameter1, parameter2)
@@ -94,6 +97,16 @@ namespace CSharpPrac
         public static int Add(int num1, int num2)
         {
             return num1 + num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double Devide(double num1, double num2)
+        {
+            return num1 / num2;
         }
     }
 }
