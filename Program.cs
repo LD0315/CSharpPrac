3d13e5e7aa43ec1504d2ecfc3f26819cd43b09c9
Linlin Deng <linlin.deng@icemedia.com.au>
2023-01-18 14:03:33 +1000

    add method

--- Program.cs
+++ Program.cs
@@ -68,8 +68,22 @@ namespace CSharpPrac
 
             // Result
             Console.WriteLine("a is: " + a1 + " and b is: " + b1);
-            
+            WriteSomething();
+            WriteSomethingSpecific("I am an argument and am called from a method");
+            Console.Read();
         }
+
+        // acess modifier (static) return type method name (parameter1, parameter2)
+        public static void WriteSomething()
+        {
+            Console.WriteLine("I am called from a method");
+        }
+
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
+        }
+
 
     }
 }
