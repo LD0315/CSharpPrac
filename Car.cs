ffe94a18ce7b59be60bfdae3020c4665f2f8e34b
Linlin Deng <linlin.deng@icemedia.com.au>
2023-01-18 12:55:38 +1000

    first

---
+++ Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CSharpPrac
+{
+    public class Car
+    {
+        public string color = "purple";
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CSharpPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string color = "blue";
+            Console.WriteLine("Hello " + color);
+
+            Console.WriteLine(2+2*6);
+
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+
+            int number = 2;
+            float price = 3.99f;
+            Console.WriteLine(number*price);
+
+            int a = 1;
+            int b = 2;
+            a = 5;
+            int c = a + b;
+            Console.WriteLine("Result is: " + c);
+
+            float vat = 19f;
+            float productA = 3.99f;
+            float productB = 11.99f;
+            float productC = 5.75f;
+
+            float summed = productA + productB + productC;
+
+            // vat is required, don't skip it
+            /*
+             * 
+             * 
+             */
+            float totalTax = (summed / 100) * vat;
+
+            summed += totalTax;
+            Console.WriteLine("Total price is: " + summed);
+
+        }
+    }
+}
